852a84cb43c67ec263d5e234f5ba5e2999b278db
Joseph Plant <git@JosephPlant.com>
2015-07-23 18:00:35 +1000

    Revert "Update xls output"
    This reverts commit f4471c88e9d72f5ab1ab3e129c48b80c27fca7be.

--- src/AnalyseProject.cmd
+++ src/AnalyseProject.cmd
@@ -57,7 +57,6 @@ del Output\Graphs\*.* /Q
 %nxslt% Working\project.links.xml StyleSheets\Project.Downtime.xslt -o Working\downtime.xml
 %nxslt% Working\downtime.xml StyleSheets\Document.Downtime.xslt -o Output\Project.Downtime.html
 %nxslt% Working\downtime.xml StyleSheets\Bootstrap.Downtime.xslt -o Output\Bootstrap.Downtime.html
-%nxslt% Working\downtime.xml StyleSheets\Excel.Downtime.xslt -o Output\Excel.Downtime.xls
 
 @echo === Interfaces ===
 %nxslt% Working\project.xml       StyleSheets\Document.Interfaces.xslt  -o Output\Project.Interfaces.html
